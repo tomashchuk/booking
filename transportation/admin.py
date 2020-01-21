87d23616de87c4d926d96a7194660e59b4d82414
DmytroTomashchuk <tomashchukdmytro@gmail.com>
2020-01-21 05:01:40 +0200

    Er

--- transportation/admin.py
+++ transportation/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import AirportsData, AircraftsData, Bookings, BoardingPasses, Flights, TicketFlights, Tickets, Seats
-
-admin.site.register(AirportsData, AircraftsData)
-admin.site.register(Bookings, BoardingPasses)
-admin.site.register(Flights, TicketFlights)
-admin.site.register(Tickets, Seats)
+#
+# admin.site.register(AirportsData, AircraftsData)
+# admin.site.register(Bookings, BoardingPasses)
+# admin.site.register(Flights, TicketFlights)
+# admin.site.register(Tickets, Seats)
