62a61f6f3cbcc2e18cc3aec6168ec431b6bb03c2
DmytroTomashchuk <tomashchukdmytro@gmail.com>
2020-01-21 05:10:34 +0200

    Added correct admin

--- transportation/admin.py
+++ transportation/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import AirportsData, AircraftsData, Bookings, BoardingPasses, Flights, TicketFlights, Tickets, Seats
-#
-# admin.site.register(AirportsData, AircraftsData)
-# admin.site.register(Bookings, BoardingPasses)
-# admin.site.register(Flights, TicketFlights)
-# admin.site.register(Tickets, Seats)
+
+admin.site.register(AirportsData)
+admin.site.register(AircraftsData)
+admin.site.register(Bookings)
+admin.site.register(BoardingPasses)
+admin.site.register(Flights)
+admin.site.register(TicketFlights)
+admin.site.register(Tickets)
+admin.site.register(Seats)
