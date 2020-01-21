bcd2e214eac4051044315a9ad8e792332f5c8c80
DmytroTomashchuk <tomashchukdmytro@gmail.com>
2020-01-21 06:30:52 +0200

    Set debug value

--- testair/settings.py
+++ testair/settings.py
@@ -55,7 +55,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['bookingstest.herokuapp.com']
 
